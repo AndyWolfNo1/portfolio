77787a8869bfb1cff93ea529408b569937c8aeb3
Niko_Kipas <36108908+AndyWolfNo1@users.noreply.github.com>
2022-01-05 22:54:21 +0100

    Delete moje.py

--- moje.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
-url = 'https://infostrefa.com/espi/pl/reports/view/4,499889'
-
-res = requests.get(url)
-soup = bs(res.text, 'html.parser')
-class_dane = soup.find_all(class_="dane")
-cd = class_dane[0].find_all(class_="nTekst")
